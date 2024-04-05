e53544a14a46fcd17d8475569263035c787b83d7
Liravictor07 <132672124+Liravictor07@users.noreply.github.com>
2024-04-05 07:55:24 -0300

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBXigqwE2-xaeUkluoeaaRH_yYGrSktfYY",
+    authDomain: "veterinaria-e1a97.firebaseapp.com",
+    projectId: "veterinaria-e1a97",
+    storageBucket: "veterinaria-e1a97.appspot.com",
+    messagingSenderId: "792073463250",
+    appId: "1:792073463250:web:6687d028d5030278fa940b"
+  };
+
+  firebase.initializeApp(firebaseConfig);
---
+++ myscript1.js
@@ -0,0 +1,68 @@
+var petNameV, ownerNameV, ownerCPFV, consultationDateV, symptomsV;
+
+function readForm() {
+    petNameV = document.getElementById("petName").value; 
+    ownerNameV = document.getElementById("ownerName").value;
+    ownerCPFV = document.getElementById("roll").value;
+    consultationDateV = document.getElementById("consultationDate").value;
+    symptomsV = document.getElementById("symptoms").value;
+    console.log(petNameV, ownerNameV, ownerCPFV, consultationDateV, symptomsV);
+}
+
+
+document.getElementById("read").onclick = function () {
+    readForm();
+
+    firebase
+    .database()
+    .ref("consultations/" + petNameV)
+    .once("value", function (snap) {
+        if (snap.exists()) {
+            var data = snap.val();
+            document.getElementById("petName").value = data.petName;
+            document.getElementById("ownerName").value = data.ownerName;
+            document.getElementById("roll").value = data.ownerCPF;
+            document.getElementById("consultationDate").value = data.consultationDate;
+            document.getElementById("symptoms").value = data.symptoms;
+        } else {
+            alert("Consulta não encontrada.");
+            clearForm();
+        }
+    });
+};
+
+document.getElementById("update").onclick = function () {
+    readForm();
+
+    firebase
+    .database()
+    .ref("consultations/" + petNameV)
+    .update({
+        petName: petNameV,
+        ownerName: ownerNameV,
+        ownerCPF: ownerCPFV,
+        consultationDate: consultationDateV,
+        symptoms: symptomsV,
+    });
+    alert("Consulta Atualizada");
+    clearForm();
+};
+
+document.getElementById("delete").onclick = function () {
+    readForm();
+
+    firebase
+    .database()
+    .ref("consultations/" + petNameV)
+    .remove();
+    alert("Consulta Cancelada");
+    clearForm();
+};
+
+function clearForm() {
+    document.getElementById("petName").value = "";
+    document.getElementById("ownerName").value = "";
+    document.getElementById("roll").value = "";
+    document.getElementById("consultationDate").value = "";
+    document.getElementById("symptoms").value = "";
+}
